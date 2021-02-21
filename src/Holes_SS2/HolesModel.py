8cf2028d988e629a1973aea4f867c6fd562f6b4f
somu15 <lakshd5@vt.edu>
2021-02-20 21:20:46 -0700

    Holes model Subset

---
+++ src/Holes_SS2/HolesModel.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 12:37:06 2021
+
+@author: dhulls
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import sys
+import csv
+sys.path.append('.')
+
+class HolesModel:
+
+    def HF(self, E1, v1, ux):
+
+        file1 = open('/home/dhullaks/projects/moose/modules/tensor_mechanics/test/tests/0_Holes/HF_R2.i', 'r')
+        Lines = file1.readlines()
+        Lines[101] = "    "+"value = '"+str(ux)+"'\n"
+        Lines[114] = "    "+"youngs_modulus = "+str(E1)+"\n"
+        Lines[115] = "    "+"poissons_ratio = "+str(v1)+"\n"
+
+        file1 = open('/home/dhullaks/projects/moose/modules/tensor_mechanics/test/tests/0_Holes/HF_R2.i', 'w')
+        file1.writelines(Lines)
+        file1.close()
+
+        os.chdir('/home/dhullaks/projects/moose/modules/tensor_mechanics/test/tests/0_Holes')
+        os.system('mpiexec -n 3 /home/dhullaks/projects/moose/modules/tensor_mechanics/tensor_mechanics-opt -i HF_R2.i')
+
+        path1 = '/home/dhullaks/projects/moose/modules/tensor_mechanics/test/tests/0_Holes/HF_R2.csv'
+        with open(path1) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=',')
+            Samp0 = []
+            count = 0
+            for row in readCSV:
+                if count > 0:
+                    Samp0.append(float(row[1]))
+                count = count + 1
+
+        stress_von = Samp0[1]
+
+        return stress_von
+
+    def HF1(self, E1, v1, ux):
+
+        file1 = open('/home/dhullaks/projects/moose/modules/tensor_mechanics/test/tests/0_Holes/HF1_R2.i', 'r')
+        Lines = file1.readlines()
+        Lines[101] = "    "+"value = '"+str(ux)+"'\n"
+        Lines[114] = "    "+"youngs_modulus = "+str(E1)+"\n"
+        Lines[115] = "    "+"poissons_ratio = "+str(v1)+"\n"
+
+        file1 = open('/home/dhullaks/projects/moose/modules/tensor_mechanics/test/tests/0_Holes/HF1_R2.i', 'w')
+        file1.writelines(Lines)
+        file1.close()
+
+        os.chdir('/home/dhullaks/projects/moose/modules/tensor_mechanics/test/tests/0_Holes')
+        os.system('mpiexec -n 3 /home/dhullaks/projects/moose/modules/tensor_mechanics/tensor_mechanics-opt -i HF1_R2.i')
+
+        path1 = '/home/dhullaks/projects/moose/modules/tensor_mechanics/test/tests/0_Holes/HF1_R2.csv'
+        with open(path1) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=',')
+            Samp0 = []
+            count = 0
+            for row in readCSV:
+                if count > 0:
+                    Samp0.append(float(row[1]))
+                count = count + 1
+
+        stress_von = Samp0[1]
+
+        return stress_von
---
+++ src/Holes_SS4/Alg_nD_Holes_LF.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 13:12:04 2021
+
+@author: dhulls
+"""
+
+from os import sys
+import os
+import pathlib
+import numpy as np
+import random
+from scipy.stats import lognorm
+from scipy.stats import norm
+from scipy.stats import rayleigh
+from scipy.stats import uniform
+from scipy.stats import cauchy
+import matplotlib.pyplot as plt
+import pickle
+
+import tensorflow.compat.v2 as tf
+import tensorflow_probability as tfp
+tfb = tfp.bijectors
+tfd = tfp.distributions
+tfk = tfp.math.psd_kernels
+tf.enable_v2_behavior()
+from LimitStateFunctions import LimitStateFunctions as LSF
+from ML_TF import ML_TF
+from DrawRandom import DrawRandom as DR
+from pyDOE import *
+
+Ndim = 3
+value = 100.0 # 600.0
+
+LS1 = LSF()
+DR1 = DR()
+num_s = 500
+
+
+uni = uniform()
+Nsub = 2000
+Psub = 0.1
+Nlim = 5
+y1 = np.zeros((Nsub,Nlim))
+y1_lim = np.zeros(Nlim)
+y1_lim[Nlim-1] = value
+inp1 = np.zeros((Nsub,Ndim,Nlim))
+rv = norm(loc=0,scale=1)
+y_seed = np.zeros(int(Psub*Nsub))
+Indicator = np.ones((Nsub,Nlim))
+
+for ii in np.arange(0,Nsub,1):
+    inp = (DR1.HolesRandom())
+    inpp = inp[None,:]
+    y1[ii,0] = np.array(LS1.Holes_HF(inpp))
+    inp1[ii,:,0] = inp
+    print(ii)
+
+inpp = np.zeros(Ndim)
+count_max = Nsub/(Psub*Nsub)
+seeds_outs = np.zeros(int(Psub*Nsub))
+seeds = np.zeros((int(Psub*Nsub),Ndim))
+markov_seed = np.zeros(Ndim)
+markov_out = 0.0
+r_sto = np.zeros((Nsub-int(Psub*Nsub),Nlim-1,Ndim))
+# ind_sto = 3
+prop_std_req =np.array([0.375,0.375,0.375])
+
+for kk in np.arange(1,Nlim,1):
+    count = np.inf
+    ind_max = 0
+    ind_sto = -1
+    seeds_outs = np.sort(y1[:,kk-1])[int((1-Psub)*Nsub):(len(y1))]
+    y1_lim[kk-1] = np.min(seeds_outs)
+    k = (y1[:,kk-1]).argsort()
+    indices = k[int((1-Psub)*Nsub):(len(y1))]
+    seeds = inp1[indices,:,kk-1]
+
+    for ii in np.arange(0,(Nsub),1):
+        print(kk)
+        print(ii)
+        nxt = np.zeros((1,Ndim))
+
+        if count > count_max:
+            ind_sto = ind_sto + 1
+            count = 0
+            markov_seed = seeds[ind_sto,:]
+            markov_out = seeds_outs[ind_sto]
+        else:
+            markov_seed = inp1[ii-1,:,kk]
+            markov_out = y1[ii-1,kk]
+
+        count = count + 1
+
+        for jj in np.arange(0,Ndim,1):
+            rv1 = norm(loc=np.log(markov_seed[jj]),scale=0.7)
+            prop = np.exp(rv1.rvs())
+            # rv1 = uniform(loc=(np.log(inp1[ind_max,jj,kk])-prop_std_req[jj]),scale=(2*prop_std_req[jj]))
+            # prop = np.exp(rv1.rvs())
+            r = np.log(DR1.HolesPDF(rv_req=(prop), index=jj)) - np.log(DR1.HolesPDF(rv_req=(markov_seed[jj]),index=jj)) # rv.pdf((prop))/rv.pdf((inp1[ii-(int(Psub*Nsub)),jj,kk]))
+            # r_sto[ii-(int(Psub*Nsub)),kk-1,jj] = r
+            if r>np.log(uni.rvs()):
+                nxt[0,jj] = prop
+            else:
+                nxt[0,jj] = markov_seed[jj]
+            inpp[jj] = nxt[0,jj]
+        y_nxt = np.array(LS1.Holes_HF(inpp[None,:])).reshape(1)
+        if y_nxt>y1_lim[kk-1]:
+            inp1[ii,:,kk] = inpp # np.array([nxt[0,0], nxt[0,1], nxt[0,2]])
+            y1[ii,kk] = y_nxt
+        else:
+            inp1[ii,:,kk] = markov_seed
+            y1[ii,kk] = markov_out
+            Indicator[ii,kk] = 0.0
+
+Pf = 1
+Pi_sto = np.zeros(Nlim)
+cov_sq = 0
+for kk in np.arange(0,Nlim,1):
+    Pi = len(np.rot90(np.where(y1[:,kk]>np.min([y1_lim[kk],value]))))/(Nsub)
+    Pf = Pf * Pi
+    Pi_sto[kk] = Pi
+    cov_sq = cov_sq + ((1-Pi)/(Pi*Nsub))
+cov_req = np.sqrt(cov_sq)
+
+filename = 'SS_Run4.pickle'
+os.chdir('/home/dhullaks/projects/Small_Pf_code/src/Holes_SS4')
+with open(filename, 'wb') as f:
+    pickle.dump(y1, f)
+    pickle.dump(y1_lim, f)
+    pickle.dump(Pf, f)
+    pickle.dump(cov_req, f)
+    pickle.dump(Nlim, f)
+    pickle.dump(Nsub, f)
+    pickle.dump(Pi_sto, f)
+    pickle.dump(Indicator, f)
